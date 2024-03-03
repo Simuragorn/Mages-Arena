7418693d149cba823ddee7e86bbda88d42869f3b
Simuragorn <simuragorn@gmail.com>
2024-03-03 15:11:03 +0200

    Update UI, add connection set up

---
+++ Assets/Scripts/Constants/ConnectionConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Constants
+{
+    public static class ConnectionConstants
+    {
+        public const string ConnectionSettingsFileName = "connection-data.txt";
+        public const string DefaultConnectionAddress = "127.0.0.1";
+        public const ushort DefaultPort = 7777;
+    }
+}
--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,29 +1,82 @@
+using Assets.Scripts.Constants;
+using Madhur.InfoPopup;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.UI;
+using static Unity.Netcode.Transports.UTP.UnityTransport;
 
 public class LobbyManager : MonoBehaviour
 {
     [SerializeField] private Transform lobbyPanel;
+    [SerializeField] private Transform scorePanel;
     [SerializeField] private Button hostButton;
     [SerializeField] private Button clientButton;
+    [SerializeField] private TMP_InputField addressInput;
+    [SerializeField] private TMP_InputField portInput;
+
+    private string ConnectionSettingsFilePath => Path.Combine(Application.dataPath, ConnectionConstants.ConnectionSettingsFileName);
     void Start()
     {
         hostButton.onClick.AddListener(StartHost);
         clientButton.onClick.AddListener(StartClient);
+
+        var connectionData = LoadConnectionData();
+        addressInput.text = connectionData.Address;
+        portInput.text = connectionData.Port.ToString();
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData = connectionData;
     }
 
     private void StartHost()
     {
+        SetConnectionData();
+
         NetworkManager.Singleton.StartHost();
         Debug.Log("HOST");
         lobbyPanel.gameObject.SetActive(false);
+        scorePanel.gameObject.SetActive(true);
     }
-    
+
     private void StartClient()
     {
+        SetConnectionData();
+
         NetworkManager.Singleton.StartClient();
         Debug.Log("CLIENT");
         lobbyPanel.gameObject.SetActive(false);
+        scorePanel.gameObject.SetActive(true);
+    }
+
+    private void SetConnectionData()
+    {
+        var connectionData = new ConnectionAddressData { Address = addressInput.text, Port = ushort.Parse(portInput.text) };
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData = connectionData;
+        SaveConnectionData(connectionData);
+    }
+
+    private ConnectionAddressData LoadConnectionData()
+    {
+        if (!File.Exists(ConnectionSettingsFilePath))
+        {
+            return new ConnectionAddressData { Address = ConnectionConstants.DefaultConnectionAddress, Port = ConnectionConstants.DefaultPort };
+        }
+        string json = File.ReadAllText(ConnectionSettingsFilePath);
+        return JsonConvert.DeserializeObject<ConnectionAddressData>(json);
+    }
+    private void SaveConnectionData(ConnectionAddressData connectionData)
+    {
+        try
+        {
+            File.WriteAllText(ConnectionSettingsFilePath, JsonConvert.SerializeObject(connectionData));
+        }
+        catch (UnauthorizedAccessException exc)
+        {
+            InfoPopupUtil.ShowAlert("Please run game with admin rights! We cannot save connection settings...");
+            throw exc;
+        }
     }
 }
